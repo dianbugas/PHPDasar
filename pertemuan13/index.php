6e699bb94c645d71acb2a3e8dd3d55523b242fe7
dianynf <dianbugas@gmail.com>
2018-11-13 15:00:01 +0700

    uplode foto

---
+++ pertemuan13/index.php
@@ -0,0 +1,63 @@
+<?php  
+require 'functions.php';
+$mahasiswa = query("SELECT * FROM mahasiswa");
+//ORDER BY id DESC
+	//koneksi ke database
+//$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+//ambil data dari tabel mahasiswa
+//$result = mysqli_query($conn, "SELECT * FROM mahasiswa");
+//
+//jika tombol cari  di tekan
+if (isset($_POST["cari"]) ){
+	$mahasiswa = cari($_POST["keyword"]);
+
+
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Admin</title>
+</head>
+<body>
+	<h1>Daftar Mahasiswa</h1>
+	<a href="tambah.php">Tambah data</a>
+	<br><br>
+	<form action="" method="post">
+		<input type="text" name="keyword" size="40" autofocus placeholder="masukkan keyword pencaharian" autocomplete="off">
+		<button type="submit" name="cari">Cari</button>
+	</form>
+	<br><br>
+	<table border="1" cellpadding="10" cellspacing="0">
+		<tr>
+			<th>No.</th>
+			<th>Aksi</th>
+			<th>Gambar</th>
+			<th>Nrp</th>
+			<th>Nama</th>
+			<th>Email</th>
+			<th>Jurusan</th>
+		</tr>
+		<?php $i=1; ?>
+		<?php foreach( $mahasiswa as $row) : ?>
+		<tr>
+			<td><?= $i; ?></td>
+			<td>
+				<a href="ubah.php?id=<?= $row["id"];?>">Ubah</a> | 
+				<a href="hapus.php?id=<?= $row["id"];?>" onclick="return confirm('yakin?');">Hapus</a>
+			</td>
+			<td>
+				<img src="img/<?= $row["gambar"];?>" whide="50">
+			</td>
+			<td><?= $row["nrp"]; ?></td>
+			<td><?= $row["nama"]; ?></td>
+			<td><?= $row["email"]; ?></td>
+			<td><?= $row["jurusan"]; ?></td>
+		</tr>
+		<?php $i++; ?>
+	<?php endforeach; ?>
+
+	</table>
+</body>
+</html>
---
+++ pertemuan13/ubah.php
@@ -0,0 +1,64 @@
+<?php 
+require 'functions.php';
+
+$id = $_GET["id"];
+	$mhs = query("SELECT * FROM mahasiswa WHERE id = $id")[0];
+if( isset($_POST["submit"]) ) {
+
+	if( ubah($_POST) > 0 ) {
+		echo"
+			<script>
+				alert('data berhasil diubah!');
+				document.location.href = 'index.php';
+			</script>
+		";
+	}else{
+		echo"
+			<script>
+				alert('data gagal diubah!');
+				document.location.href = 'index.php';
+			</script>
+		";
+	}
+	
+}
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ubah data mahasiswa</title>
+</head>
+<body>
+	<h1>Ubah data</h1>
+	<form action="" method="post" enctype="multipart/form-data">
+		<input type="hidden" name="id" value="<?= $mhs["id"]; ?>">
+		<input type="hidden" name="gambarLama" value="<?= $mhs["gambar"]; ?>">
+		<ul>
+			<li>
+				<label for="nrp">NRP :</label>
+				<input type="text" name="nrp" id="nrp" required value="<?= $mhs["nrp"]; ?>">
+			</li>
+			<li>
+				<label for="nama">Nama :</label>
+				<input type="text" name="nama" id="nama" required value="<?= $mhs["nama"]; ?>">
+			</li>
+			<li>
+				<label for="email">Email :</label>
+				<input type="text" name="email" id="email" required value="<?= $mhs["email"]; ?>">
+			</li>
+			<li>
+				<label for="jurusan">Jurusan :</label>
+				<input type="text" name="jurusan" id="jurusan" required value="<?= $mhs["jurusan"]; ?>">
+			</li>
+			<li>
+				<label for="gambar">Gambar :</label><br>
+				<img src="img/<?= $mhs['gambar']; ?>" widht="40"><br>
+				<input type="file" name="gambar" id="gambar" required">
+			</li>
+			<li>
+				<button type="submit" name="submit">Edit data!</button>
+			</li>
+		</ul>
+	</form>
+</body>
+</html>
