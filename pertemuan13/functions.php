dea2bc625911ac39828ec27f83eb8e1eacb18c69
dianynf <dianbugas@gmail.com>
2018-11-14 09:09:39 +0700

    login

--- pertemuan13/functions.php
+++ pertemuan13/functions.php
@@ -124,9 +124,29 @@ function registrasi($data){
 	$password = mysqli_real_ascape_string($conn, $data["password"]);
 	$password2 = mysqli_real_ascape_string($conn, $data["password2"]);
 
+	//cek username sudah ada atau belum
+	mysqli_query($conn, "SELECT username FROM user WHERE
+		username = '$username'");
+	if(mysqli_fetch_assoc($result)){
+		echo"<script>
+			alert('username sudah terdaftar!')
+			</script>";
+			return false;
+	}
+
 	//cek konfirmasi password
-	if ($password !== $password2)
+	if ($password !== $password2) {
+		echo "<script>
+		alert('konfirmasi password tidak sesuai!');
+		</script>";
+		return false;
+	}
+	//enkripsi password
+	$password = password_hash($password, PASSWORD_DEFAULT);
+
+	//tambahkan userbaru ke database
+	mysql_query($conn, "INSERT INTO user VALUES('', 'username', 'password')");
 }
 
 
-?>
+?> 
